ceab0edd2e9a409b9a8335c955858180c82a6483
rias-z <chestermx88@gmail.com>
2018-08-11 21:34:07 +0900

    feat|fix: flip詳細画面の改良

--- src/pages/FlipDetail/index.js
+++ src/pages/FlipDetail/index.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+// components
+import FlipDetailSideBar from '../../components/FlipDetailSideBar'
+import FlipHeader from '../../components/FlipHeader'
+import FlipMainView from '../../components/FlipMainView'
+import FlipTagList from '../../components/FlipTagList'
+
+
 // logic
 import {
   deleteFlip,
@@ -10,6 +17,9 @@ import {
   revertFlipBookmark,
   revertFlipGood,
 } from './logic'
+
+// styles
+import { StyledFlipDetail } from './styles';
 
 
 class FlipDetail extends Component {
@@ -21,108 +31,39 @@ class FlipDetail extends Component {
 
   render() {
     if (this.props.isLoading) {
-      const flip = this.props.flipDetail.flip
-      const author = this.props.flipDetail.author
-      const items = this.props.flipDetail.items
+      const { author, flip, items, tags } = this.props.flipDetail
+      const { isBookmark, isGood } = this.props
 
       return (
         <div className="FlipDetail">
-          --- FlipDetail ---
-          <br />
+          <FlipDetailSideBar
+            flip={flip}
+            isBookmark={isBookmark}
+            isGood={isGood}
+            postFlipBookmark={() => this.props.postFlipBookmark(flip.flip_id)}
+            postFlipGood={() => this.props.postFlipGood(flip.flip_id)}
+            revertFlipBookmark={() => this.props.revertFlipBookmark(flip.flip_id)}
+            revertFlipGood={() => this.props.revertFlipGood(flip.flip_id)}
+          />
 
-          {(() => {
-            if (this.props.isAuthor) {
-              return (
-                <div>
-                  <h3>Flip作成者メニュー</h3>
-                  <input type='button' value='削除する' onClick={(e) => {
-                    e.preventDefault()
-                    this.props.deleteFlip(flip.flip_id, this.props.username)
-                  }} />
-                </div>
-                )
-            } else if (this.props.isAuthenticated) {
-              return (
-                <div>
-                  <h3>ログインメニュー</h3>
-                  <input type='button' value='編集する' onClick={(e) => {
-                    e.preventDefault()
-                    this.props.history.push(flip.flip_id + '/edit')
-                  }} />
-                </div>
-              )
-            }
-          })()}
+          <StyledFlipDetail>
 
-          <br />
-          Author:<br/>
-          [{author.user_id}] {author.username} {author.thumbnail}<br />
-          <br />
+            {/* 作成者ユーザ */}
+            <FlipHeader
+              author={author}
+              flip={flip}
+            />
+            <FlipTagList
+              tags={tags}
+            />
 
-          Flip:<br />
-          [{flip.flip_id}] {flip.title}<br />
-          bmk: {flip.bookmark_cnt} gb: {flip.good_cnt} create_at: ({flip.create_at})<br />
-          <br />
+            {/* メインビュー */}
+            <FlipMainView
+              flip={flip}
+              items={items}
+            />
 
-          Items:<br />
-          {items.map(item => (
-            <div key={item.item_id}>
-              [{item.item_id}] {item.name}<br />
-              url: {item.url}<br />
-              description: {item.description}<br />
-              <br />
-            </div>
-          ))}
-
-          {(() => {
-            if (this.props.isAuthenticated) {
-              const renderFlipGoodButton = !this.props.isGood ? (
-                <div>
-                  いいね：{this.props.goodCnt}_
-                  <input type='button' value='いいねする' onClick={() => {
-                    this.props.postFlipGood(flip.flip_id)
-                  }} />
-                </div>
-              ) : (
-                <div>
-                  いいね：{this.props.goodCnt}_
-                  <input type='button' value='いいねを取り消す' onClick={() => {
-                    this.props.revertFlipGood(flip.flip_id)
-                  }} />
-                </div>
-              )
-
-              const renderFlipBookmarkButton = !this.props.isBookmark ? (
-                <div>
-                  ブックマーク：{this.props.bookmarkCnt}_
-                  <input type='button' value='ブックマークする' onClick={() => {
-                    this.props.postFlipBookmark(flip.flip_id)
-                  }} />
-                </div>
-              ) : (
-                <div>
-                  ブックマーク：{this.props.bookmarkCnt}_
-                  <input type='button' value='ブックマークを取り消す' onClick={() => {
-                    this.props.revertFlipBookmark(flip.flip_id)
-                  }} />
-                </div>
-              )
-
-              return (
-                <div>
-                  {renderFlipGoodButton}
-                  {renderFlipBookmarkButton}
-                </div>
-              )
-            } else {
-              return (
-                <div>
-                  いいね： {this.props.goodCnt}<br />
-                  ブックマーク： {this.props.bookmarkCnt}<br />
-                </div>
-              )
-            }
-          })()}
+          </StyledFlipDetail>
         </div>
       )
     } else {
---
+++ src/pages/FlipDetail/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+
+export const StyledFlipDetail = styled.div`
+  width: 50rem;
+  min-height: 40rem;
+  margin: 0 auto;
+  background-color: #fff;
+`
