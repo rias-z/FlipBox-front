6511064f186b5a65239038b221496d809551eb15
rias-z <chestermx88@gmail.com>
2018-08-11 21:37:22 +0900

    feat: flip詳細画面 SideBar追加

---
+++ src/components/FlipDetailSideBar/index.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Div = styled.div`
+  position: absolute;
+  position: -webkit-sticky;
+  position: sticky;
+  top: 15rem;
+`
+
+const Img = styled.img`
+  padding: 0 0 0 2rem;
+  width: 2.4rem;
+  height: 2.4rem;
+`
+
+// TODO: SideBarのdivが食い込んでいるので，間隔調整を行う必要アリ
+const FlipDetailSideBar = (props) => {
+  const { flip, isBookmark, isGood } = props
+  const {
+    postFlipBookmark,
+    postFlipGood,
+    revertFlipBookmark,
+    revertFlipGood,
+  } = props
+
+  console.log("isBookmark", isBookmark)
+  console.log("isGood", isGood)
+
+  const bookmarkButton = isBookmark ? (
+    <div>
+      <Img
+        src={'/images/bookmark-on.png'}
+        onClick={revertFlipBookmark}
+      /> {flip.bookmark_cnt}
+    </div>
+  ) : (
+    <div>
+      <Img
+        src={'/images/bookmark-off.png'}
+        onClick={postFlipBookmark}
+      /> {flip.bookmark_cnt}
+    </div>
+  )
+
+  const favoriteButton = isGood ? (
+    <div>
+      <Img
+        src={'/images/favorite-on.png'}
+        onClick={revertFlipGood}
+      /> {flip.good_cnt}
+    </div>
+  ) : (
+    <div>
+      <Img
+        src={'/images/favorite-off.png'}
+        onClick={postFlipGood}
+      /> {flip.good_cnt}
+    </div>
+  )
+
+  return (
+    <Div className='FlipDetailSideBar'>
+      {favoriteButton}
+      {bookmarkButton}
+    </Div>
+  )
+}
+
+export default FlipDetailSideBar
--- src/containers/App/styles.js
+++ src/containers/App/styles.js
@@ -2,5 +2,6 @@ import styled from 'styled-components'
 
 
 export const WrapperMain = styled.div`
-  margin-top: 4rem;
+  // TODO: SideBarのdivが食い込んでいるので，一時コメントアウト
+  // padding: 4rem 0;
 `
